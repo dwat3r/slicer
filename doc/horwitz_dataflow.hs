af61be9e7ecd22f54e7b0f2580d4221d9a15b9f4
dwat3r <hechtloliver@gmail.com>
2017-04-19 18:23:05 +0200

    dataflow

--- doc/horwitz_dataflow.hs
+++ doc/horwitz_dataflow.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Control.Monad.State
 
 newtype Var  = Var {getName :: String}
   deriving (Eq, Show)
@@ -21,24 +22,28 @@ type Criterion = (Stmt,[Var])
 -- directly relevant variables and statements with 0
 -- indirectly relevant variables and statements with > 0
 rj :: Int -> Criterion -> Stmt -> [Stmt] -> [Var]
-rj k c i ss = concat [r k c j ss | j <- edges i]
+rj k c i ss = concat [r k c j ss (i:vs) | j <- edges i]
 
 -- i -cfg-> j
-r :: Int -> Criterion -> Stmt -> [Stmt] -> [Var]
-r 0 c i ss = initi ++ [v | v <- rj 0 c i ss, v `notElem` def i ] ++ 
-          [v | v <- ref i, not $ null (def i `intersect` rj 0 c i ss) ]
-          where initi | i == fst c = snd c
-                      | otherwise  = []
+-- it, crit, curstmt, visitedstmts, stmts , affectedvars
+r :: Int -> Criterion -> Stmt -> [Stmt] -> [Stmt] -> [Var]
+r 0 c i ss vs | i `elem` vs = []
+              | otherwise   =  initi ++ [v | v <- rj 0 c i ss (i:vs), v `notElem` def i ] ++ 
+                            [v | v <- ref i, not $ null (def i `intersect` rj 0 c i ss (i:vs)) ]
+                            where initi | i == fst c = snd c
+                                        | otherwise  = []
 
-r k c i ss = r (k-1) c i ss ++ nub (concat [r 0 (b,ref b) i ss | b <- b (k-1) c ss])
+r k c i ss vs | i `elem` vs = []
+              | otherwise   = r (k-1) c i ss (i:vs) ++ nub (concat [r 0 (b,ref b) i ss (i:vs) | b <- b (k-1) c ss vs])
 
 --todo recursion to eliminate infinite loops
-s :: Int -> Criterion -> [Stmt] -> [Stmt]
-s 0 c ss = [i | i <- ss, not $ null (def i `intersect` rj 0 c i ss)]
-s k c ss = b (k-1) c ss ++ [i | i <- ss, not $ null (def i `intersect` rj k c i ss)]
+s :: Int -> Criterion -> [Stmt] -> [Stmt] -> [Stmt]
+s 0 c ss vs = [i | i <- ss, not $ null (def i `intersect` rj 0 c i ss vs)]
+s k c ss vs = b (k-1) c ss ++ [i | i <- ss, not $ null (def i `intersect` rj k c i ss (i:vs))]
 
-b :: Int -> Criterion -> [Stmt] -> [Stmt]
-b k c ss = [bi | bi <- s k c ss,not $ null $ infl bi]
+b :: Int -> Criterion -> [Stmt] -> [Stmt] -> [Stmt]
+b k c ss vs = [bi | bi <- s k c ss vs,not $ null $ infl bi]
+
 
 -- sample data from horwitz:
 vi = Var "i"
@@ -47,10 +52,42 @@ n0 = Stmt {text = "int sum = 0;",   def = [vsum], ref = [],     infl = [],     e
 n1 = Stmt {text = "int i = sum;",   def = [vi],   ref = [vsum], infl = [],     edges = [n2]}
 n2 = Stmt {text = "while (i < 11)", def = [],     ref = [vi],   infl = [n3,n4],edges = [n3]}
 n3 = Stmt {text = "sum += i;",      def = [vsum], ref = [vi],   infl = [],     edges = [n4]}
-n4 = Stmt {text = "i++",            def = [vi],   ref = [vi],   infl = [],     edges = []}
+n4 = Stmt {text = "i++",            def = [vi],   ref = [vi],   infl = [],     edges = [n2]}
 
 horwitz :: [Stmt]
 horwitz = [n0,n1,n2,n3,n4]
 
 c :: Criterion
 c = (n4,[vi])
+
+{-
+todo.
+type Iteration = Int
+type VisitAmount = Int
+type SStat = (([(Stmt,VisitAmount)],Criterion,Iteration),[Stmt])
+type SVal  = [Stmt]
+
+r :: Stmt -> State SStat SVal
+r i = do
+  (cur@(ss,c,it),res) <- get
+  case it of
+    0 -> put (cur,initi ++ )
+    _ -> 
+    where initi | i == fst c = snd c
+              | otherwise  = []
+
+s :: State SStat SVal
+s = do
+  (ss,_,_) <- get
+
+b :: State SStat SVal
+b = do
+
+
+
+computeSlice :: [Stmt] -> State SStat SVal
+computeSlice = 
+
+
+
+-}
